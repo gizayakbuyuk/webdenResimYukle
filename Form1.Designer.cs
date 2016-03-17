75f84c3ed194bbbf0571f340bcc1e44128ebdc35
gizayakbuyuk <gizaykata@gmail.com>
2016-03-17 19:15:59 +0200

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Picturebox_a_webden_resim_yüklemek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "http://ismek.ibb.gov.tr/";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(374, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Resimleri Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(591, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(365, 486);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(704, 181);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(214, 69);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 85);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(573, 413);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 48);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(573, 33);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 510);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace Picturebox_a_webden_resim_yüklemek
+{
+    public partial class Form1 : Form
+    {
+        static string kaynakKoduGetir(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            using (StreamReader sRead = new StreamReader(resp.GetResponseStream(), Encoding.UTF8))
+            {
+                return sRead.ReadToEnd();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void kucukResimOlustur(string yol)
+        {
+            try
+            {
+                PictureBox kResim = new PictureBox();
+                kResim.Load(yol);
+                kResim.Width = 100;
+                kResim.Height = 100;
+                kResim.SizeMode = PictureBoxSizeMode.StretchImage;
+                //kResim.BorderStyle = BorderStyle.FixedSingle;
+                kResim.Tag = yol;
+                kResim.Click += new EventHandler(kResim_Click);
+                kResim.MouseHover += new EventHandler(kResim_Üzerinde);
+                flowLayoutPanel1.Controls.Add(kResim);
+            }
+            catch  { }
+            
+        }
+        private void kResim_Üzerinde(object sender, EventArgs e)
+        {
+            for (int i = 0; i < flowLayoutPanel1.Controls.Count; i++)
+                ((PictureBox)flowLayoutPanel1.Controls[i]).BorderStyle = BorderStyle.None;
+            ((PictureBox)sender).BorderStyle = BorderStyle.FixedSingle;
+
+        }
+        private void kResim_Click(object sender, EventArgs e)
+        {
+            string yol=((PictureBox)sender).Tag.ToString();
+            pictureBox1.Load(yol);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                //pictureBox1.Load(textBox1.Text);
+                string html= kaynakKoduGetir(textBox1.Text);
+                listBox1.Items.Clear();
+                flowLayoutPanel1.Controls.Clear();
+                progressBar1.Maximum = html.Length;
+                progressBar1.Value = 0;
+
+                while (true)
+                {
+                    int imgbul = html.IndexOf("<img")+4;
+                    if (imgbul == 3) break;
+                    int srcbul=html.IndexOf("src=",imgbul)+5;
+                    if (srcbul == 4) break;
+                    int trnkbul = html.IndexOf('"', srcbul );
+                    if (trnkbul == -1) break;
+                    string yol = html.Substring(srcbul, trnkbul - srcbul);
+                    if (yol != "")
+                    {
+                        if (yol.StartsWith("http://") == false)
+                            yol = textBox1.Text + yol;
+                        if (listBox1.Items.IndexOf(yol) == -1)
+                        {
+                            listBox1.Items.Add(yol);
+                            kucukResimOlustur(yol);
+                        }
+                    }
+                    html = html.Substring(trnkbul, html.Length - trnkbul);
+                    progressBar1.Value = progressBar1.Maximum - html.Length;
+                    Application.DoEvents();
+                }
+                progressBar1.Value = 0;
+                
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Load(listBox1.Text);
+            }
+            catch (Exception)
+            {
+                
+                
+            }
+            
+        }
+    }
+}
